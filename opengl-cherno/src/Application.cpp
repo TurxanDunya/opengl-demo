6515728239676eb73fe393a3c051e3083913ab26
Turxan Dunyamaliyev <50988453+TurxanDunya@users.noreply.github.com>
2023-10-10 22:55:41 +0400

    some huge files excluded

--- opengl-cherno/src/Application.cpp
+++ opengl-cherno/src/Application.cpp
@@ -18,6 +18,9 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
+#include "lmgui/imgui.h"
+#include "lmgui/imgui_impl_glfw_gl3.h"
+
 int main(void)
 {
     GLFWwindow* window;
@@ -71,18 +74,12 @@ int main(void)
 
         IndexBuffer ib(indices, 6);
 
-
         Shader shader("res/shader/basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.0, 1.0, 0.0, 1.0);
 
         glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.f, -1.0f, 1.0f);
         glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
-        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 0, 0));
-
-        glm::mat4 mvp = proj * view * model;
-
-        shader.SetuniformMat4f("u_MVP", mvp);
 
         Texture texture("res/textures/crusader.png");
         texture.Bind();
@@ -95,14 +92,26 @@ int main(void)
 
         Renderer renderer;
 
+        ImGui::CreateContext();
+        ImGui_ImplGlfwGL3_Init(window, true);
+        ImGui::StyleColorsDark();
+
+        glm::vec3 translation(200, 0, 0);
+
         float r = 0.0f;
         float increment = 0.05f;
         while (!glfwWindowShouldClose(window))
         {
             GLCall(renderer.Clear());
 
+            ImGui_ImplGlfwGL3_NewFrame();
+
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+            glm::mat4 mvp = proj * view * model;
+
             shader.Bind();
             shader.SetUniform4f("u_Color", r, 1.0, 0.0, 1.0);
+            shader.SetuniformMat4f("u_MVP", mvp);
 
             GLCall(renderer.Draw(va, ib, shader));
 
@@ -113,6 +122,14 @@ int main(void)
 
             r += increment;
 
+            {
+                ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);              
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            }
+
+            ImGui::Render();
+            ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
 
@@ -121,6 +138,9 @@ int main(void)
         }
     }
 
+    ImGui_ImplGlfwGL3_Shutdown();
+    ImGui::DestroyContext();
     glfwTerminate();
+
     return 0;
 }
